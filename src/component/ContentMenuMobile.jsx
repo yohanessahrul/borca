0f0d690090b7b154b296497b448b2baea927518e
yohanessahrul <yohanessahrul92@gmail.com>
2018-10-08 02:41:00 +0700

    header mobile

---
+++ src/component/ContentMenuMobile.jsx
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Container, Row, Col} from 'reactstrap';
+
+class ContentMenuMobile extends Component {
+  render() {
+    return (
+      <div style={{ width: '100%' }}>
+        <div className="menu-mobile-wrap">
+          <div style={{ display: 'table', margin: '0 auto', fontSize: '76px', marginBottom: '-30px' }} className="logo-mobile">BORCA</div>
+          <ul>
+            <hr/>
+            <li>New Arrival</li>
+            <li>Top</li>
+            <li>Bottom</li>
+            <hr/>
+            <li>Cara Order</li>
+            <li>Resi Pengiriman</li>
+            <li>Konfirmasi Pembayaran</li>
+            <li>Kontak</li>
+            <hr/>
+          </ul>
+        </div>
+        
+      </div>
+    );
+  }
+}
+
+export default ContentMenuMobile;
--- src/component/HeaderMobile.jsx
+++ src/component/HeaderMobile.jsx
@@ -1,10 +1,70 @@
 import React, { Component } from 'react';
+import { Container, Row, Col} from 'reactstrap';
+
+import { Icon } from 'react-icons-kit';
+import {ic_menu, ic_close} from 'react-icons-kit/md';
+
+import ContentMenuMobile from '../component/ContentMenuMobile';
 
 class HeaderMobile extends Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      showMenu: false
+    }
+    this.showMenu = this.showMenu.bind(this);
+  }
+  showMenu () {
+    console.log('show menu')
+    this.setState({
+      showMenu: !this.state.showMenu
+    })
+  }
   render() {
+    const menuShowHide = () => {
+      if (this.state.showMenu) {
+        return (
+          <div className="menu">
+            <div className="bg-menu-mobile">
+              <Container>
+                <Row>
+                  <Col xs="12">
+                    <Icon onClick={this.showMenu} style={{ color: 'white', float: 'right', padding: '10px', background: '#1e1e1e' }} icon={ic_close} size={35}/>
+                  </Col>
+                </Row>
+                <Row>
+                  <Col xs="12">                  
+                    <ContentMenuMobile/> 
+                  </Col>
+                </Row>
+              </Container>
+            </div>
+          </div>
+        )
+      } else {
+        return ''
+      }
+    }
     return (
-      <div>
-        <h1>Header Mobile</h1>
+      <div style={{ width: '100%', background: 'black' }}>
+        <Container>
+          <Row>
+            <Col xs="8">
+              <div style={{ width: '100%' }}>
+                <div style={{ float: 'left' }}>
+                  <Icon onClick={this.showMenu} style={{ padding: '10px', display: 'table', color: 'white' }} icon={ic_menu} size={35}/>
+                </div>
+                <div>
+                  <h1 className="logo-mobile">BORCA</h1>
+                </div>
+              </div>
+            </Col>
+            <Col xs="4">
+              {/* <div style={{ width: '100%', height: '50px', background: 'yellow' }}></div> */}
+            </Col>
+          </Row>
+        </Container>
+        {menuShowHide()}
       </div>
     );
   }
