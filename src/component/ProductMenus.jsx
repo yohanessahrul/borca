ed9fa4e4a12f93fc7f674cf368c41763e3fabb55
yohanessahrul <yohanessahrul92@gmail.com>
2018-10-11 17:04:02 +0700

    update build

--- src/component/ProductMenus.jsx
+++ src/component/ProductMenus.jsx
@@ -5,7 +5,7 @@ import BottomMenu from '../component/BottomMenu';
 class ProductMenus extends Component {
   render() {
     return (
-    <div style={{ position: 'absolute', width: '100%', display: 'block', background: 'white', zIndex: '10', left: 0, top: 129, padding: '10px', border: 'thin solid #efefef' }}>
+    <div style={styles.productMenuWrapper}>
       <ul style={{ padding: 0, margin: 0 }}>
         <li style={{ listStyle: 'none', width: '50%', height: '300px', float: 'left'}}>
           <TopMenu/>
@@ -17,6 +17,25 @@ class ProductMenus extends Component {
       </ul>
     </div>
     );
+  }
+}
+
+const styles = {
+  productMenuWrapper: {
+    position: 'absolute',
+    width: '100%',
+    display: 'block',
+    background: 'white',
+    zIndex: '10',
+    left: 0,
+    top: 129,
+    padding: '10px',
+    border: 'thin solid #efefef',
+    background: 'rgb(255,255,255)',
+    background: '-moz-linear-gradient(top, rgba(255,255,255,1) 0%, rgba(249,244,255,1) 100%)',
+    background: '-webkit-linear-gradient(top, rgba(255,255,255,1) 0%,rgba(249,244,255,1) 100%)',
+    background: 'linear-gradient(to bottom, rgba(255,255,255,1) 0%,rgba(249,244,255,1) 100%)',
+    filter: 'progid:DXImageTransform.Microsoft.gradient( startColorstr="#ffffff", endColorstr="#f9f4ff",GradientType=0 )'
   }
 }
 
